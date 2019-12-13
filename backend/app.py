ad454489272202a77af16253c31d741dde4cd9ad
hazelwww <hazelwang0729@gmail.com>
2019-12-13 00:51:24 +0000

    Added lobby

--- backend/app.py
+++ backend/app.py
@@ -3,11 +3,15 @@ from google.cloud import datastore
 from auth import auth
 from calender import calendar
 from flask_cors import CORS
+from lobby import lobby
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
 app.register_blueprint(auth, url_prefix='/auth')
 app.register_blueprint(calendar, url_prefix='/calendar')
+app.register_blueprint(lobby, url_prefix='/lobby')
 CORS(app)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
+
--- backend/lobby.py
+++ backend/lobby.py
@@ -1,1 +1,24 @@
 # lobby api - socket
+from flask_socketio import SocketIO
+from flask import Flask, Blueprint
+
+lobby = Blueprint('lobby', __name__)
+lobby = SocketIO(lobby)
+
+@lobby.on('create')
+def on_join(data):
+    userid = data['userid']
+    room = data['eventid']
+    join_room(room)
+    send(, room=room)
+
+@lobby.on('leave')
+def on_leave(data):
+    userid = data['userid']
+    room = data['eventid']
+    leave_room(room)
+    send(, room=room)
+
+
+
+
