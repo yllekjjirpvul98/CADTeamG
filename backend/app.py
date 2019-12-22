f8a9ee94ebb9333452aa22384f5c6696ef0630e2
Luke <luke.gibson98@hotmail.com>
2019-12-22 15:58:59 +0000

    Revert "Edits to get working"
    This reverts commit b1a3884f840ae2c6bd002a00eaf52e6ae6f9835b.

--- backend/app.py
+++ backend/app.py
@@ -1,17 +1,17 @@
 from flask import Flask
-from flask_session import Session
+from flask.ext.session import Session
 from google.cloud import datastore
 from auth import auth
 from event import event
 from session import session, sio
 from flask_cors import CORS
 import socketio
-from session import sio
+from lobby import sio
 
 app = Flask(__name__)
 app.register_blueprint(auth, url_prefix='/auth')
 app.register_blueprint(event, url_prefix='/events')
-app.register_blueprint(session, url_prefix='/session')
+app.register_blueprint(lobby, url_prefix='/session')
 app.wsgi_app = socketio.WSGIApp(sio, app.wsgi_app)
 SESSION_TYPE = 'redis'
 app.config.from_object(__name__)
@@ -20,4 +20,3 @@ CORS(app)
 
 if __name__ == '__main__':
     app.run(threaded=True, port=8080)
-    
